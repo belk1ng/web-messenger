387485b30f163aa8d2140a2b3ac551a2fbd20eab
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-04-10 18:55:23 +0300

    feat: 🎸 add Loader component

--- src/index.tsx
+++ src/index.tsx
@@ -9,12 +9,15 @@ import "./assets/styles/index.scss";
 import Router from "./routes/Router";
 import { BrowserRouter } from "react-router-dom";
 
+import Loader from "./components/loader/Loader";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <BrowserRouter>
-    <Suspense fallback={<h1>Loading...</h1>}>
+    <Suspense fallback={<Loader />}>
       <React.StrictMode>
         <Router />
       </React.StrictMode>
