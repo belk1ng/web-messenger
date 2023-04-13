118606f1b020a0674f3b88711ffb0a095702339b
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-04-13 13:17:18 +0300

    refactor: 💡 add catcher decorator for catch failed api requests

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,32 +1,19 @@
-import instance from "./axios";
-import axios from "axios";
+import axios from "./axios";
+import catcher from "./catcher";
 import { ServerResponse } from "../@types/api";
 import { LoginValues } from "../@types/auth";
 
 class AuthAPI {
   static AUTH_PREFIX = "/auth";
 
+  @catcher
   static async login<T = string>(
     values: LoginValues
   ): Promise<ServerResponse<T> | undefined> {
-    try {
-      const response = await instance.post(
-        `${AuthAPI.AUTH_PREFIX}/signin`,
-        values
-      );
-      const { data, status } = response;
+    const response = await axios.post(`${AuthAPI.AUTH_PREFIX}/signin`, values);
+    const { data, status } = response;
 
-      return { data, status };
-    } catch (error: unknown) {
-      if (axios.isAxiosError(error)) {
-        return {
-          data: error.response?.data as T,
-          status: error?.response?.status as number,
-        };
-      } else {
-        console.log("Unexpecred error: ", error);
-      }
-    }
+    return { data, status };
   }
 }
 
---
+++ src/api/catcher.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { BadRequestError } from "../@types/api";
+
+function catcher(_: object, __: string, descriptor: PropertyDescriptor) {
+  const method = descriptor.value;
+
+  descriptor.value = async function (...args: unknown[]) {
+    try {
+      const response = await method.apply(this, args);
+      return response;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log("AxiosError: ", error.response);
+        return {
+          data: error.response?.data as BadRequestError,
+          status: error?.response?.status as number,
+        };
+      } else {
+        console.log("Unexpecred error: ", error);
+      }
+    }
+  };
+}
+
+export default catcher;
