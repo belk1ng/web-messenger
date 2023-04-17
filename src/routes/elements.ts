8720700339149edf167730bc71b87578a4978886
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-04-17 15:51:39 +0300

    fix: 🐛 profile import #2

--- src/routes/elements.ts
+++ src/routes/elements.ts
@@ -3,7 +3,7 @@ import { lazy } from "react";
 export const LoginPage = lazy(() => import("../pages/Login"));
 export const RegistrationPage = lazy(() => import("../pages/Registration"));
 export const ProfilePage = lazy(
-  () => import("../pages/Profile/ProfileContent")
+  () => import("../pages/profile/ProfileContent")
 );
 export const ProfileEditPage = lazy(() => import("../pages/ProfileEdit"));
 export const ProfileChangePasswordPage = lazy(
