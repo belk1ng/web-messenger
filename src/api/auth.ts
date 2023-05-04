034d8494c2cd475aa23e6e23c455b3063c2d7730
Dmitry Belkin <dmitriy.belkin.2001@mail.ru>
2023-05-04 15:12:47 +0300

    refactor: 💡 cather decorator to return { data, status }

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -13,46 +13,32 @@ class AuthAPI {
 
   @catcher
   static async login(values: LoginValues): MethodResponse<string> {
-    const response = await axios.post<string>(
+    return axios.post<string>(
       `${AuthAPI.AUTH_PREFIX}${AUTH_ENDPOINTS.LOGIN}`,
       values
     );
-    const { data, status } = response;
-
-    return { data, status };
   }
 
   @catcher
   static async register(
     values: RegistrationValues
   ): MethodResponse<Pick<AuthUser, "id">> {
-    const response = await axios.post<Pick<AuthUser, "id">>(
+    return axios.post<Pick<AuthUser, "id">>(
       `${AuthAPI.AUTH_PREFIX}${AUTH_ENDPOINTS.REGISTRATION}`,
       values
     );
-    const { data, status } = response;
-
-    return { data, status };
   }
 
   @catcher
   static async logout(): MethodResponse<string> {
-    const response = await axios.post<string>(
-      `${AuthAPI.AUTH_PREFIX}${AUTH_ENDPOINTS.LOGOUT}`
-    );
-    const { data, status } = response;
-
-    return { data, status };
+    return axios.post<string>(`${AuthAPI.AUTH_PREFIX}${AUTH_ENDPOINTS.LOGOUT}`);
   }
 
   @catcher
   static async getUser(): MethodResponse<AuthUser> {
-    const response = await axios.get<AuthUser>(
+    return axios.get<AuthUser>(
       `${AuthAPI.AUTH_PREFIX}${AUTH_ENDPOINTS.USER_INFO}`
     );
-    const { data, status } = response;
-
-    return { data, status };
   }
 }
 
--- src/api/chats.ts
+++ src/api/chats.ts
@@ -8,22 +8,16 @@ class ChatsAPI {
 
   @catcher
   static async getChats(): MethodResponse<Chat[]> {
-    const response = await axios.get<Chat[]>(
+    return axios.get<Chat[]>(
       `${ChatsAPI.CHATS_PREFIX}${CHATS_ENDPOINTS.CHATS}`
     );
-    const { data, status } = response;
-
-    return { data, status };
   }
 
   @catcher
   static async getToken(id: number): MethodResponse<{ token: string }> {
-    const response = await axios.post<{ token: string }>(
+    return axios.post<{ token: string }>(
       `${ChatsAPI.CHATS_PREFIX}/token/${id}`
     );
-    const { data, status } = response;
-
-    return { data, status };
   }
 }
 
